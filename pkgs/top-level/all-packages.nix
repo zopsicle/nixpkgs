d586e603ffce8b26eeea9b6885cc76d2f957aa8b
Robin Gloster <mail@glob.in>
2017-04-29 12:38:59 +0200

    libexosip: add patch for openssl 1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11037,9 +11037,7 @@ in
 
   libexif = callPackage ../development/libraries/libexif { };
 
-  libexosip = callPackage ../development/libraries/exosip {
-    openssl = openssl_1_0_2;
-  };
+  libexosip = callPackage ../development/libraries/exosip {};
 
   libextractor = callPackage ../development/libraries/libextractor {
     libmpeg2 = mpeg2dec;
