45b41b4ab3876a9311e5b496593f7312b2a0200b
Andreas Rammhold <andreas@rammhold.de>
2019-03-05 00:32:41 +0100

    monero: lift boost override
    The newer boost libraries support openssl 1.1 out of the box and
    removing the override doesn't seem to break the application either.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18484,7 +18484,6 @@ in
 
   monero = callPackage ../applications/altcoins/monero {
     inherit (darwin.apple_sdk.frameworks) CoreData IOKit PCSC;
-    boost = boost15x;
   };
 
   monero-gui = libsForQt5.callPackage ../applications/altcoins/monero-gui {
