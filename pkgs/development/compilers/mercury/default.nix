2764f7757a2caca9ae0b1ba8eff3eff2c123e542
Chloé Kekoa <chloekek@use.startmail.com>
2022-04-23 11:58:45 +0200

    mercury: build bootstrapping phase in separate derivation
    This avoids excessive rebuilds when building different grade sets.

--- pkgs/development/compilers/mercury/default.nix
+++ pkgs/development/compilers/mercury/default.nix
@@ -3,64 +3,75 @@
 , erlangGrade ? false
 , javaGrade ? false }:
 
-stdenv.mkDerivation rec {
-  pname = "mercury";
-  version = "22.01";
+let
+  # Mercury is a self-hosting compiler with a bootstrap phase.
+  # By compiling the bootstrap phase in a separate derivation,
+  # we avoid excessive rebuilds when building different grade sets.
+  mercury = { bootstrap }:
+    stdenv.mkDerivation rec {
+      pname = "mercury";
+      version = "22.01";
 
-  src = fetchurl {
-    url    = "https://dl.mercurylang.org/release/mercury-srcdist-${version}.tar.gz";
-    sha256 = "sha256-FL4xQzAs+7djg9rGJua/ZFOMhHSCiVleaWYxlHl3XUc=";
-  };
+      src = fetchurl {
+        url    = "https://dl.mercurylang.org/release/mercury-srcdist-${version}.tar.gz";
+        sha256 = "sha256-FL4xQzAs+7djg9rGJua/ZFOMhHSCiVleaWYxlHl3XUc=";
+      };
 
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [ gcc flex bison texinfo readline ]
-    ++ lib.optionals erlangGrade [ erlang ]
-    ++ lib.optionals javaGrade [ jdk ];
+      nativeBuildInputs = [ makeWrapper ];
+      buildInputs = [ gcc flex bison texinfo readline ]
+        ++ lib.optionals (!bootstrap) [ (mercury { bootstrap = true; }) ]
+        ++ lib.optionals (!bootstrap && erlangGrade) [ erlang ]
+        ++ lib.optionals (!bootstrap && javaGrade) [ jdk ];
 
-  patchPhase = ''
-    # Fix calls to programs in /bin
-    for p in uname pwd ; do
-      for f in $(egrep -lr /bin/$p *) ; do
-        sed -i 's@/bin/'$p'@'$p'@g' $f ;
-      done
-    done
-  '';
+      patchPhase = ''
+        # Fix calls to programs in /bin
+        for p in uname pwd ; do
+          for f in $(egrep -lr /bin/$p *) ; do
+            sed -i 's@/bin/'$p'@'$p'@g' $f ;
+          done
+        done
+      '';
 
-  preConfigure = ''
-    mkdir -p $out/lib/mercury/cgi-bin ;
-    configureFlags="--enable-deep-profiler=$out/lib/mercury/cgi-bin";
-  '';
+      configureFlags =
+        lib.optionals bootstrap [ "--enable-minimal-install" ];
 
-  preBuild = ''
-    # Mercury buildsystem does not take -jN directly.
-    makeFlags="PARALLEL=-j$NIX_BUILD_CORES" ;
-  '';
+      preConfigure = ''
+        mkdir -p $out/lib/mercury/cgi-bin ;
+        configureFlags="$configureFlags --enable-deep-profiler=$out/lib/mercury/cgi-bin";
+      '';
 
-  postInstall = ''
-    # Wrap with compilers for the different targets.
-    for e in $(ls $out/bin) ; do
-      wrapProgram $out/bin/$e \
-        --prefix PATH ":" "${gcc}/bin" \
-        ${if javaGrade then ''--prefix PATH ":" "${jdk}/bin"'' else ""} \
-        ${if erlangGrade then ''--prefix PATH ":" "${erlang}/bin"'' else ""}
-    done
-  '';
+      preBuild = ''
+        # Mercury buildsystem does not take -jN directly.
+        makeFlags="PARALLEL=-j$NIX_BUILD_CORES" ;
+      '';
 
-  meta = {
-    description = "A pure logic programming language";
-    longDescription = ''
-      Mercury is a logic/functional programming language which combines the
-      clarity and expressiveness of declarative programming with advanced
-      static analysis and error detection features.  Its highly optimized
-      execution algorithm delivers efficiency far in excess of existing logic
-      programming systems, and close to conventional programming systems.
-      Mercury addresses the problems of large-scale program development,
-      allowing modularity, separate compilation, and numerous optimization/time
-      trade-offs.
-    '';
-    homepage    = "http://mercurylang.org";
-    license     = lib.licenses.gpl2;
-    platforms = lib.platforms.linux ++ lib.platforms.darwin;
-    maintainers = [ ];
-  };
-}
+      postInstall = ''
+        # Wrap with compilers for the different targets.
+        for e in $(ls $out/bin) ; do
+          wrapProgram $out/bin/$e \
+            --prefix PATH ":" "${gcc}/bin" \
+            ${if !bootstrap && javaGrade then ''--prefix PATH ":" "${jdk}/bin"'' else ""} \
+            ${if !bootstrap && erlangGrade then ''--prefix PATH ":" "${erlang}/bin"'' else ""}
+        done
+      '';
+
+      meta = {
+        description = "A pure logic programming language";
+        longDescription = ''
+          Mercury is a logic/functional programming language which combines the
+          clarity and expressiveness of declarative programming with advanced
+          static analysis and error detection features.  Its highly optimized
+          execution algorithm delivers efficiency far in excess of existing logic
+          programming systems, and close to conventional programming systems.
+          Mercury addresses the problems of large-scale program development,
+          allowing modularity, separate compilation, and numerous optimization/time
+          trade-offs.
+        '';
+        homepage    = "http://mercurylang.org";
+        license     = lib.licenses.gpl2;
+        platforms = lib.platforms.linux ++ lib.platforms.darwin;
+        maintainers = [ ];
+      };
+    };
+in
+  mercury { bootstrap = false; }
